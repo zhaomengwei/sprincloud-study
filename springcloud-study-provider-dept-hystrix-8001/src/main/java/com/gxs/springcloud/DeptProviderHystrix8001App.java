45099262f9d14bef75501907d27197a20f97cb31
zpj80231 <916665067@qq.com>
2019-11-02 18:33:47 +0800

    Hystrix服务熔断

--- springcloud-study-provider-dept-hystrix-8001/src/main/java/com/gxs/springcloud/DeptProviderHystrix8001App.java
+++ springcloud-study-provider-dept-hystrix-8001/src/main/java/com/gxs/springcloud/DeptProviderHystrix8001App.java
@@ -12,10 +12,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @description
  */
 @SpringBootApplication
-//本服务启动后会自动注册进eureka服务
-@EnableEurekaClient
-@EnableDiscoveryClient
-@EnableCircuitBreaker
+@EnableEurekaClient//本服务启动后会自动注册进eureka服务
+@EnableDiscoveryClient//服务发现
+@EnableCircuitBreaker//对Hystrix熔断机制的支持
 public class DeptProviderHystrix8001App {
 
     public static void main(String[] args) {
